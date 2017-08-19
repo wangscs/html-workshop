d1c3e875d2108e296416d80009f34796ae9969cc
Dev <dev@divethree.com>
2017-08-18 23:41:46 -0700

    Make non-js page.

--- exercises/music_boxes/static/js/index.js
+++ exercises/music_boxes/static/js/index.js
@@ -6,26 +6,3 @@ function playNote(e) {
     note.currentTime = 0;
     note.play();
 }
-
-
-
-/*
-// Using JQuery
-$(document).ready( function() {
-
-   // This controls the button for the c note.
-
-   // This variable calls the cNote audio element in the HTML.
-   var cNote = document.getElementById('cAudio');
-
-   // This turns our <div id= "c"> into a button that plays our audio file when the box is pressed.
-   $('#c').mousedown(function(){
-
-       // This is a property that scrubs the audio file back to its start.
-       cNote.currentTime = 0;
-
-       // This plays the audio file.
-       cNote.play();
-   });
-});
-**/
