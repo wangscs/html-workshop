8360d474ac199c13f493d83ce7a78bceb5b0426a
Okinz <steven14812@gmail.com>
2021-04-19 16:04:16 -0400

    Added game design and started functionality

--- exercises/memory/game.js
+++ exercises/memory/game.js
@@ -8,6 +8,27 @@
  *  - Only 2 cards can be face-up at once.
  */
 window.onload = function() {
-
+    // console.log("here");
+    // test();
+  let len = 16
+  for(let i = 0; i < len; i++){
+    document.querySelectorAll(".box")[i].addEventListener("click", function(){
+      this.className = " reveal";
+    });
+  }
 }
+
+function test() {
+    console.log("here");
+    document.querySelector(".box").className = "reveal";
+    let allCards = document.querySelectorAll(".box");
+    for(let i = 0; i < 16; i++){
+        console.log(allCards[i]);
+        allCards[i].className = "reveal";
+    }
+}
+//   for(let i = 0; i < allCards.length; i++){
+//     allCards[i].style.display = "block";
+//   }
+
 
